21049ca13068f682ffd360012642c95bbed5c4e9
Victor <santtosvictor42@gmail.com>
2022-07-19 15:43:51 -0300

    http post, cadastrar produto

---
+++ cadastrar-produto.js
@@ -0,0 +1,27 @@
+const http = require('http')
+
+const options = {
+    hostname: "localhost",
+    port: 3000,
+    path: '/produtos',
+    method: 'POST',
+    headers: {
+        'Accept': "application/json",
+        'Content-type':'application/json'
+    }
+}
+const client = http.request(options, (res) => {
+    console.log(res.statusCode);
+    
+    res.on('data', function(body){
+        console.log(`Body ${body}`)
+    })
+})
+
+const produto = {
+    titulo: "PEQUENO PRINCIPE",
+    descricao: "um pequeno principew",
+    valor: '50'
+}
+
+client.end(JSON.stringify(produto))
